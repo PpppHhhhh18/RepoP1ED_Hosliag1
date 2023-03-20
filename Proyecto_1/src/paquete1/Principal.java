2a54c167c180470aa69656f87e7f3bf4e6571c77
HP <HP@DESKTOP-PDO0K13>
2023-03-20 18:27:29 +0100

    Tercera modificacion del programa

--- Proyecto_1/src/paquete1/Principal.java
+++ Proyecto_1/src/paquete1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Programa para control de versiones con Git");
 		System.out.println("Modificacion secundaria de la clase.");
+		System.out.println("Esta edicion es para practicar el uso de 'commit'.");
 	}
 
 }
